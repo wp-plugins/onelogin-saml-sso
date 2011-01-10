f1cb76d41c71d7034de321403850207830918cc8
onelogin <onelogin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-10 22:07:09 +0000

    Adding first version
    git-svn-id: https://plugins.svn.wordpress.org/onelogin-saml-sso/trunk@331008 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ onelogin-saml-sso/saml_consumer.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: OneLogin SAML SSO
+Plugin URI: http://support.onelogin.com/entries/383540
+Description: Give users secure one-click access to WordPress from OneLogin. This SAML integration eliminates passwords and allows you to authenticate users against your existing Active Directory or LDAP server as well increase security using YubiKeys or VeriSign VIP Access, browser PKI certificates and OneLogin's flexible security policies. OneLogin is pre-integrated with thousands of apps and handles all of your SSO needs in the cloud and behind the firewall.
+Tags: saml, sso, authentication, password, security, yubikey, ldap, activedirectory
+Author: OneLogin, Inc.
+Version: 1.0.0
+Author URI: http://www.onelogin.com
+*/
+
+	// check for posted SAML Response
+	if (isset($_POST["SAMLResponse"])) {
+		add_action('init','saml_authenticate_hook');
+	}
+
+	function saml_authenticate_hook()
+	{
+		require_once(ABSPATH . '/wp-content/plugins/onelogin-saml-sso/php/authenticate.php');
+		saml_authenticate();
+	}
+
+	// add menu option for configuration
+	add_action('admin_menu', 'onelogin_saml_menu');
+
+	function onelogin_saml_menu() {
+		require_once(ABSPATH . '/wp-content/plugins/onelogin-saml-sso/php/configuration.php');
+		onelogin_saml_configuration();
+	}
+
+?>
